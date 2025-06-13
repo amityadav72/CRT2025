b99f879a2d02b1ec081e60db3019cd926d4913db
Amit Kumar Yadav <988981amityadav@gmail.com>
2025-06-13 23:27:03 +0530

    initial commit

---
+++ Day5/loopdemo.java
@@ -0,0 +1,12 @@
+package CRT2025.Day5;
+
+public class loopdemo {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=50;i++){
+            sum+=i;
+
+        }
+        System.out.println(sum);
+    }
+}
