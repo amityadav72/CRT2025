ae9265c241a99bf8b9adc88e341fafe38581bf63
Amit Kumar Yadav <988981amityadav@gmail.com>
2025-06-14 13:44:16 +0530

    initial commit

---
+++ Day6/parametrizedConstructorDemo.java
@@ -0,0 +1,13 @@
+package CRT2025.Day6;
+
+public class parametrizedConstructorDemo {
+
+    parametrizedConstructorDemo(int a, int b){
+        System.out.println("Inside the parametrizedConstructorDemo");
+    }
+
+    public static void main(String[] args) {
+        parametrizedConstructorDemo obj = new parametrizedConstructorDemo(10,20);
+
+    }
+}
