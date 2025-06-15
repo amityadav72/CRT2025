acf38aa87f654f2545975e1ccbe9c86c8ef68795
Amit Kumar Yadav <988981amityadav@gmail.com>
2025-06-15 13:21:51 +0530

    initial commit

---
+++ Day7/garbagecollection/GarbageCollectionExplicit.java
@@ -0,0 +1,15 @@
+package CRT2025.Day7.garbagecollection;
+
+public class GarbageCollectionExplicit {
+    public void test(){
+        System.out.println("test method ");
+
+
+    }
+
+    public static void main(String[] args) {
+
+        GarbageCollectionExplicit obj = new GarbageCollectionExplicit();
+        obj.test();
+    }
+}
