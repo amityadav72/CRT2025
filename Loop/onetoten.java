dfce038cafad9228836e223ddd55436820a179a7
Amit Kumar Yadav <988981amityadav@gmail.com>
2025-06-17 15:15:11 +0530

    CRT-2025

---
+++ Loop/onetoten.java
@@ -0,0 +1,11 @@
+package CRT2025.Loop;
+
+public class onetoten {
+
+        public static void main(String[] args) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
